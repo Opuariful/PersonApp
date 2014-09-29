442c5717ee6ae78006699b9307d72d03365d5226
Opuariful <oopu.ariful@gmail.com>
2014-09-30 05:28:31 +0600

    Person Application.

---
+++ PersonApp/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    class Person
+    {
+        public string FirstName { private get; set; }
+        public string MiddleName { private get; set; }
+        public string LastName { private get; set; }
+
+        public Person(string firstName, string middleName, string lastName): this (firstName,lastName)
+        {
+            MiddleName = middleName;
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + MiddleName + " " + LastName;
+        }
+
+                  
+
+        public string GetFullReversName()
+        {
+            string fullName = GetFullName();
+            string reverseName = "";
+            for (int index = fullName.Length - 1; index >= 0; index--)
+               {
+                  reverseName += fullName[index];
+               }
+            
+                return reverseName;
+            }
+
+        }
+
+    }
+
+
+
---
+++ PersonApp/PersonUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonApp
+{
+    public partial class PersonUI : Form
+    {
+        public PersonUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            Person anPerson = new Person(firstNameTextBox.Text, middlaNameTextBox.Text,lastNameTextBox.Text);
+            Person aPerson = new Person(firstNameTextBox.Text, lastNameTextBox.Text);
+
+            fullNameTextBox.Text = anPerson.GetFullName();
+            reversNameTextBox.Text = anPerson.GetFullReversName();
+        }
+    }
+}
